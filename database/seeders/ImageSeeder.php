a17cee3b452e433f8b4781d3fb0c5c3790100e60
JSimko4 <xsimkoj2@stuba.sk>
2021-11-16 15:38:56 +0100

    uprava databazy pre obrazky, specifikacia produktu s obrazkami z databazy, viac produktov v seederi

---
+++ database/seeders/ImageSeeder.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace Database\Seeders;
+
+use Carbon\Carbon;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class ImageSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $images = array(
+            array(
+                'product_id' => 1,
+                'filename' => 'acer.jpg',
+                'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
+                'updated_at' => Carbon::now()->format('Y-m-d H:i:s')
+            ),
+            array(
+                'product_id' => 1,
+                'filename' => 'ntb4.jpg',
+                'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
+                'updated_at' => Carbon::now()->format('Y-m-d H:i:s')
+            ),
+            array(
+                'product_id' => 1,
+                'filename' => 'ntb.jpg',
+                'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
+                'updated_at' => Carbon::now()->format('Y-m-d H:i:s')
+            ),
+            array(
+                'product_id' => 1,
+                'filename' => 'ntb2.jpg',
+                'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
+                'updated_at' => Carbon::now()->format('Y-m-d H:i:s')
+            ),
+            array(
+                'product_id' => 1,
+                'filename' => 'ntb3.jpg',
+                'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
+                'updated_at' => Carbon::now()->format('Y-m-d H:i:s')
+            ),
+            array(
+                'product_id' => 1,
+                'filename' => 'ntb5.jpg',
+                'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
+                'updated_at' => Carbon::now()->format('Y-m-d H:i:s')
+            ),
+            array(
+                'product_id' => 2,
+                'filename' => 'ntb6.png',
+                'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
+                'updated_at' => Carbon::now()->format('Y-m-d H:i:s')
+            ),
+            array(
+                'product_id' => 3,
+                'filename' => 'ntbpink.jpg',
+                'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
+                'updated_at' => Carbon::now()->format('Y-m-d H:i:s')
+            ),
+        );
+
+        foreach ($images as $image){
+            DB::table('images')->insert($image);
+        }
+    }
+}
