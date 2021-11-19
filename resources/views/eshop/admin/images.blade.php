2954127a0712f33de07a1df6ebb5230c398186a4
JSimko4 <xsimkoj2@stuba.sk>
2021-11-19 14:13:02 +0100

    fotky edit front

--- resources/views/eshop/admin/images.blade.php
+++ resources/views/eshop/admin/images.blade.php
@@ -2,11 +2,15 @@
 
 @section('content')
     <div class="container-fluid my-5">
-        <section  class="row d-flex flex-column mx-5 px-3" style="border: 2px solid black">
+        <section  class="row d-flex flex-column mx-5 px-3">
             <h1 class="text-center mt-4">Fotky</h1>
             @foreach ($product->images as $image)
-                <article class="p-3 my-2" style="border: 1px solid black">
-                    <img style="height: 150px;" src="/img/{{$image->filename}}" alt="fotka produktu">
+                <article class="p-3 my-2 d-flex justify-content-around" style="border: 2px solid black">
+                    <img class="col-3" style="height: 150px;" src="/img/{{$image->filename}}" alt="fotka produktu">
+                    <div class="col-7 d-flex justify-content-center align-items-center">
+                        <a class="btn btn-secondary btn-sm mt-3 col-3 mx-5" href=""><i class="bi bi-pencil-square"></i> Upraviť</a>
+                        <a class="btn btn-danger btn-sm mt-3 col-3" href=""><i class="fa fa-trash-o"></i>Odstrániť</a>
+                    </div>
                 </article>
             @endforeach
         </section>
