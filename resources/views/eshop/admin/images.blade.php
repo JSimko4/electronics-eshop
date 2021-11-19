c269b0c1f5c4504ec2e468678e5ee01a0c4d597f
JSimko4 <xsimkoj2@stuba.sk>
2021-11-19 14:16:22 +0100

    pridat dalsie fotky frontend

--- resources/views/eshop/admin/images.blade.php
+++ resources/views/eshop/admin/images.blade.php
@@ -2,9 +2,10 @@
 
 @section('content')
     <div class="container-fluid my-5">
-        <section  class="row d-flex flex-column mx-5 px-3">
+        <section  class="row d-flex flex-column align-items-center mx-5 px-3">
             <h1 class="text-center mt-4">Fotky</h1>
-            @foreach ($product->images as $image)
+            <a class="btn btn-success btn-sm col-3 mb-4 mt-2" href=""><i class="bi bi-images me-2"></i>Pridať ďalšie fotky</a>
+        @foreach ($product->images as $image)
                 <article class="p-3 my-2 d-flex justify-content-around" style="border: 2px solid black">
                     <img class="col-3" style="height: 150px;" src="/img/{{$image->filename}}" alt="fotka produktu">
                     <div class="col-7 d-flex justify-content-center align-items-center">
